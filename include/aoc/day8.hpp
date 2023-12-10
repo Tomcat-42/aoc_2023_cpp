a8ec2722ef129d36b66978776b78dd7558b5d03b
Pablo <PabloASHugen@protonmail.com>
2023-12-09 21:17:53 -0300

    Day 8: done

--- include/aoc/day8.hpp
+++ include/aoc/day8.hpp
@@ -4,9 +4,10 @@
 #include <format>
 #include <functional>
 #include <istream>
+#include <map>
 #include <ranges>
+#include <regex>
 #include <utility>
-#include <unordered_set>
 
 namespace aoc::day8 {
   enum class instruction : char {
@@ -17,22 +18,14 @@ namespace aoc::day8 {
   auto operator>>(std::istream& is, aoc::day8::instruction& i) -> std::istream&;
   auto operator<<(std::ostream& os, aoc::day8::instruction i) -> std::ostream&;
 
-  template <typename T>
   struct graph {
-    std::pair<T,
-              std::pair<std::shared_ptr<graph<T>>, std::shared_ptr<graph<T>>>>
-      root;
-
-    graph(T const& value, std::shared_ptr<graph<T>> left = nullptr,
-          std::shared_ptr<graph<T>> right = nullptr)
-      : root { value, { std::move(left), std::move(right) } } {}
-    using entry = std::pair<T, std::pair<T, T>>;
+    std::map<std::string, std::pair<std::string, std::string>> nodes;
+    using entry = std::pair<std::string, std::pair<std::string, std::string>>;
 
     static auto from_entries(std::ranges::range auto&& entries) -> graph;
-    [[nodiscard]] auto search(T const& t) const
-      -> std::optional<std::shared_ptr<graph<T>>>;
-    [[nodiscard]] auto traverse(std::ranges::range auto&& instructions, T begin,
-                                T end) const -> std::optional<std::uint32_t>;
+    [[nodiscard]] auto traverse(std::ranges::range auto&& instructions,
+                                std::regex begin, std::regex end) const
+      -> std::uint64_t;
   };
 
   auto solution(std::istream& input)
@@ -42,52 +35,21 @@ namespace aoc::day8 {
 } // namespace aoc::day8
 
 namespace std {
-  auto operator>>(std::istream& is, aoc::day8::graph<std::string>::entry& e)
+  auto operator>>(std::istream& is, aoc::day8::graph::entry& e)
     -> std::istream&;
-  auto operator<<(std::ostream& os, aoc::day8::graph<std::string>::entry e)
-    -> std::ostream&;
 
-  template <typename T>
-  using dag = aoc::day8::graph<T>;
+  auto operator<<(std::ostream& os, aoc::day8::graph::entry e) -> std::ostream&;
 
-template <typename T>
-struct std::formatter<dag<T>> {
+  template <>
+  struct std::formatter<aoc::day8::graph> {
     constexpr auto parse(std::format_parse_context& ctx) { return ctx.begin(); }
 
     template <typename FormatContext>
-    auto format(const dag<T>& g, FormatContext& ctx) const {
-        std::unordered_set<const dag<T>*> visited;
-        std::function<std::string(const dag<T>&, const std::string&)> walk =
-            [&walk, &visited](const dag<T>& graph, const std::string& prefix) -> std::string {
-            if (visited.find(&graph) != visited.end()) {
-                // Node already visited, return to avoid infinite loop
-                return std::format("{}[...]\n", prefix);
-            }
-            visited.insert(&graph); // Mark this node as visited
-
-            std::string s;
-            auto left = graph.root.second.first;
-            auto right = graph.root.second.second;
-
-            if (left) {
-                auto name = left->root.first;
-                s += std::format("{}├──\033[33m{}\033[0m<\033[34m{:p}\033[0m>\n",
-                                 prefix, name, static_cast<void*>(left.get()));
-                s += walk(*left, std::format("{}│  ", prefix));
-            }
-            if (right) {
-                s += std::format("{}└──\033[33m{}\033[0m<\033[34m{:p}\033[0m>\n",
-                                 prefix, right->root.first,
-                                 static_cast<const void*>(right.get()));
-                s += walk(*right, std::format("{}   ", prefix));
-            }
-
-            return s;
-        };
-        return std::format_to(
-            ctx.out(), "\033[32m{}\033[0m<\033[34m{:p}\033[0m>\n{}", g.root.first,
-            static_cast<const void*>(&g), walk(g, ""));
+    auto format(const aoc::day8::graph& g, FormatContext& ctx) const {
+      return std::formatter<
+               std::map<std::string, std::pair<std::string, std::string>>>()
+        .format(g.nodes, ctx);
     }
-};
+  };
 
 } // namespace std
--- src/aoc/day8.cpp
+++ src/aoc/day8.cpp
@@ -1,137 +1,59 @@
+#include <algorithm>
 #include <aoc/day8.hpp>
 
 #include <cstdint>
-#include <cstdio>
-#include <format>
-#include <ios>
 #include <iostream>
 #include <istream>
-#include <memory>
 #include <numeric>
-#include <optional>
 #include <print>
 #include <ranges>
 #include <regex>
 #include <sstream>
-#include <stack>
-#include <utility>
+#include <string>
+#include <vector>
 
 namespace aoc::day8 {
-  template <typename T>
-  auto graph<T>::from_entries(std::ranges::range auto&& entries) -> graph<T> {
-    return std::accumulate(
+  auto graph::from_entries(std::ranges::range auto&& entries) -> graph {
+    auto nodes_map = std::accumulate(
       std::ranges::begin(entries), std::ranges::end(entries),
-      graph<T> { T {}, nullptr, nullptr },
-      [](graph<T>&& acc, graph<T>::entry const& entry) -> graph<T> {
+      std::map<std::string, std::pair<std::string, std::string>> {},
+      [](auto&& acc, graph::entry const& entry) {
         auto const& [name, children] = entry;
-        auto& [left, right] = children;
-
-        // seach for the node with the name
-        auto g = acc.search(name);
-
-        // if the node exists, we update its children
-        if (g) {
-          auto left_node = acc.search(left);
-          if (left_node) {
-            (*g)->root.second.first = *left_node;
-          } else {
-            (*g)->root.second.first = std::make_shared<graph<T>>(left);
-          }
-
-          auto right_node = acc.search(right);
-          if (right_node) {
-            (*g)->root.second.second = *right_node;
-          } else {
-            (*g)->root.second.second = std::make_shared<graph<T>>(right);
-          }
-
-        } else {
-          acc.root.first = name;
-          acc.root.second.first = std::make_shared<graph<T>>(left);
-          if (left != right) {
-            acc.root.second.second = std::make_shared<graph<T>>(right);
-          } else {
-            acc.root.second.second = acc.root.second.first;
-          }
-        }
-
+        acc[name] = children;
         return std::move(acc);
       });
+
+    return { .nodes = std::move(nodes_map) };
   }
 
-  template <typename T>
-  auto graph<T>::search(T const& t) const
-    -> std::optional<std::shared_ptr<graph<T>>> {
-    std::unordered_set<const graph<T>*> visited;
-    std::stack<std::shared_ptr<graph<T>>> s;
-    s.push(std::make_shared<graph<T>>(*this));
-
-    while (!s.empty()) {
-      auto const curr = s.top();
-      s.pop();
-
-      if (visited.find(curr.get()) != visited.end()) {
-        continue;
-      }
-      visited.insert(curr.get());
-
-      if (curr->root.first == t) {
-        return curr;
-      }
-
-      if (curr->root.second.first) {
-        s.push(curr->root.second.first);
-      }
-
-      if (curr->root.second.second) {
-        s.push(curr->root.second.second);
-      }
-    }
-
-    return std::nullopt;
-  }
-
-  template <typename T>
-  auto graph<T>::traverse(std::ranges::range auto&& instructions, T begin,
-                          T end) const -> std::optional<std::uint32_t> {
-
-    // std::unordered_set<const graph<T>*> visited;
-    std::stack<std::pair<std::shared_ptr<graph<T>>, std::uint32_t>> s;
-    std::uint32_t path_length = 0;
-
-    auto node = search(begin);
-    if (!node) {
-      return std::nullopt;
-    }
-
-    s.push({ *node, path_length });
-    while (!s.empty()) {
-      auto const [curr, curr_path_length] = s.top();
-      s.pop();
-
-      // if (visited.find(curr.get()) != visited.end()) {
-      //   continue;
-      // }
-      // visited.insert(curr.get());
-
-      if (curr->root.first == end) {
-        return curr_path_length;
-      }
-      switch (instructions[curr_path_length % instructions.size()]) {
-      case instruction::left:
-        if (curr->root.second.first) {
-          s.push({ curr->root.second.first, curr_path_length + 1 });
+  auto graph::traverse(std::ranges::range auto&& instructions, std::regex begin,
+                       std::regex end) const -> std::uint64_t {
+    auto steps_from_to = [&](auto from, auto to) {
+      std::uint64_t steps = 0;
+      while (!std::regex_match(from, to)) {
+        switch (instructions[steps++ % instructions.size()]) {
+        case instruction::left:
+          from = std::get<0>(nodes.at(from));
+          break;
+        case instruction::right:
+          from = std::get<1>(nodes.at(from));
+          break;
         }
-        break;
-      case instruction::right:
-        if (curr->root.second.second) {
-          s.push({ curr->root.second.second, curr_path_length + 1 });
-        }
-        break;
       }
-    }
 
-    return std::nullopt;
+      return steps;
+    };
+
+    auto steps = nodes | std::views::keys | std::views::filter([&](auto node) {
+                   return std::regex_match(node, begin);
+                 }) |
+      std::views::transform(
+                   [&](auto node) { return steps_from_to(node, end); }) |
+      std::ranges::to<std::vector<int>>();
+
+    return std::accumulate(
+      std::ranges::begin(steps), std::ranges::end(steps), 1ull,
+      [](auto m, auto n) { return m / std::gcd(m, n) * n; });
   }
 
   auto solution(std::istream& input)
@@ -139,18 +61,20 @@ namespace aoc::day8 {
                  std::function<std::string()>> const {
     const auto instructions = std::ranges::views::istream<instruction>(input) |
       std::ranges::to<std::vector<instruction>>();
-    const auto entries =
-      std::ranges::views::istream<graph<std::string>::entry>(input) |
-      std::ranges::to<std::vector<graph<std::string>::entry>>();
-    const auto g = graph<std::string>::from_entries(entries);
-    std::println("{}", g);
+    const auto entries = std::ranges::views::istream<graph::entry>(input) |
+      std::ranges::to<std::vector<graph::entry>>();
+    const auto g = graph::from_entries(entries);
 
     const auto pt1 = [=]() -> std::string {
-      return std::to_string(g.traverse(instructions, "AAA", "ZZZ").value_or(0));
+      return std::to_string(
+        g.traverse(instructions, std::regex { "AAA" }, std::regex { "ZZZ" }));
     };
 
-    const auto pt2 = []() -> std::string {
-      return "not implemented yet";
+    const auto pt2 = [=]() -> std::string {
+      return std::to_string(
+        g.traverse(instructions, std::regex { ".*A$" }, std::regex { ".*Z$" }));
+
+      return "not implemented";
     };
 
     return { pt1, pt2 };
@@ -185,7 +109,7 @@ namespace aoc::day8 {
 } // namespace aoc::day8
 
 namespace std {
-  auto operator>>(std::istream& is, aoc::day8::graph<std::string>::entry& e)
+  auto operator>>(std::istream& is, aoc::day8::graph::entry& e)
     -> std::istream& {
     std::string line, name, left, right;
 
@@ -206,7 +130,7 @@ namespace std {
     return is;
   }
 
-  auto operator<<(std::ostream& os, aoc::day8::graph<std::string>::entry e)
+  auto operator<<(std::ostream& os, aoc::day8::graph::entry e)
     -> std::ostream& {
     return os << e.first << " = (" << e.second.first << ", " << e.second.second
               << ")";
